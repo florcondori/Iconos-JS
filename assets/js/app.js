6c409e8cafd5dd92d28c6bbc6b2cf9b12d0fda79
flor condori <condorihuamanif@gmail.com>
2017-04-20 15:33:23 -0500

    creacion de elementos mediante html

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function(){
+	var array = [{icon:"icon-star",h2:"Sigue tu Estrella",h5:"presente",descripcion:"Solo debes mirar atras para ver lo lejos que haz llegado"},
+				{icon:"icon-diamond",h2:"¿Conoces Ruby?",h5:"Lenguaje de Programacion",descripcion:"Sus fans lo ven como un lenguaje hermoso y artistico, dicen que es practico y ameno"},
+				{icon:"icon-eye",h2:"Mirad la Vida",h5:"Meditar",descripcion:"No todos los ojos cerrados duermen, Ni todos los ojos abiertos ven"},
+				{icon:"icon-music",h2:"Lo bueno esta",h5:"Musica",descripcion:"A veces hay que inyectarse un poco de fantasia para no morir de realidad"},
+				{icon:"icon-gift",h2:"Lo que recibes",h5:"Dar",descripcion:"Amigos son aquellos que te ven llorando y dicen ¿A QUIEN MATAMOS?"}];
+});
